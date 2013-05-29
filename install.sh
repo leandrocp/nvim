ffbc79e87119d201849d9afd8167da734f330cae
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2013-05-29 11:49:49 +0100

    vim install

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# https://github.com/thoughtbot/dotfiles/blob/master/install.sh
+
+function install() {
+  declare -a files=(vimrc gvimrc mswinrc)
+
+  for name in ${files[@]}; do
+    target="$HOME/.$name"
+
+    rm -rf "$target"
+    if [[ "$OSTYPE" == "cygwin" ]]; then
+      echo "cp -R "$PWD/$name" "$target""
+      cp -R "$PWD/$name" "$target"
+    else
+      echo "ln -s "$PWD/$name" "$target""
+      ln -s "$PWD/$name" "$target"
+    fi
+  done
+}
+
+read -p "This action will replace all target files. Are you sure? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  install
+fi
